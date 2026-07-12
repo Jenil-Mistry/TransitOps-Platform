5e7258009aef81a26094323275afc9fb1f5c20cb
Shreyansh-90 <physcoim00o@gmail.com>
2026-07-12 12:02:54 +0530

    BIKE added to schema

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,3 +2,13 @@ const express = require('express')
 
 const app = express()
 
+app.get('/', (req, res) => {
+    res.send("This is the backend server!")
+})
+
+app.post('/', (req, res) => {
+    
+    res.send("This is the backend server!")
+})
+
+module.exports = app;
